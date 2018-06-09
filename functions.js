22143ccb07fe5ec1cc694f4e534248f90dcaaf36
Jamie Frady <jamiefrady@Jamies-MacBook-Pro.local>
2018-06-08 18:11:42 -1000

    functions added

--- functions.js
+++ functions.js
@@ -3,20 +3,30 @@
  * @param {number} n
  * @return {string} the number as a string
  */
-
+function numberToString (n) {
+    return n.toString();
+}
+var n = 15;
 
 /**
  * Adds one to a given number.
  * @param {number} n
  * @return {number}
  */
-
+function increase (num) {
+    return num + 1;
+}
+var num = 15;
 
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
+function decrease (dnum) {
+    return dnum - 1;
+}
+var dnum = 15;
 
 
 /**
@@ -25,7 +35,11 @@
  * @param {number} y
  * @return {number} the sum
  */
-
+function add (adnum, addnum) {
+    return adnum + addnum;
+}
+var adnum = 15;
+var addnum = 12;
 
 /**
  * Subtracts the second number from the first.
@@ -33,7 +47,11 @@
  * @param {number} y
  * @return {number} the difference
  */
-
+function subtract (subnum, subbnum) {
+    return subnum - subbnum;
+}
+var subnum = 15;
+var subbnum = 12;
 
 /**
  * Multiplies two numbers.
@@ -41,7 +59,11 @@
  * @param {number} y
  * @return {number} the product
  */
-
+function multiply (multinum, multiinum) {
+    return multinum * multiinum;
+}
+var multinum = 15;
+var multiinum = 12;
 
 /**
  * Divides the first number by the second.
@@ -49,14 +71,21 @@
  * @param {number} y
  * @return {number} the quotient
  */
-
+function divide (divnum, divvnum) {
+    return divnum / divvnum;
+}
+var divnum = 15;
+var divvnum = 3;
 
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
-
+function square (squnum){
+    return squnum * squnum;
+}
+var squnum = 11;
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -66,6 +95,29 @@
  * @param {number} y
  * @return {number} the result
  */
+function calculate (operation, x, y) {
+    if (operation === "add"){
+    var result = x+y;
+    console.log(x + " + " + y + " = " + result);
+    return result;
+    }
+    else if (operation === "subtract"){
+    var result = x-y;
+    console.log(x + " - " + y + " = " + result);
+    return result;
+    }
+    else if (operation === "multiply"){
+    var result = x*y;
+    console.log(x + " * " + y + " = " + result);
+    return result;
+    }
+    else if (operation === "divide"){
+    var result = x/y;
+    console.log(x + " / " + y + " = " + result);
+    return result;
+    }
+    
+}
 
 
 /**
@@ -74,7 +126,14 @@
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
-
+function isGreaterThan (a,b) {
+    if (a>b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 /**
  * Returns true if `a` is less than `b`.
@@ -82,6 +141,14 @@
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
+function isLessThan (a,b) {
+    if (a<b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
 /**
@@ -90,6 +157,12 @@
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+function areEqual(a,b){
+    if (a === b) {
+        return true;
+    }
+    else {return false;}
+}
 
 
 /**
@@ -98,7 +171,14 @@
  * @param {number} y
  * @return {number} the smallest number
  */
-
+function minimum (x,y) {
+    if (x > y) {
+        return y;
+    }
+    else {
+        return x;
+    }
+}
 
 /**
  * Returns the largest value of two numbers.
@@ -106,20 +186,42 @@
  * @param {number} y
  * @return {number} the largest number
  */
-
+function maximum (x,y){
+    if (x>y) {
+        return x;
+    }
+    else {
+        return y;
+    }
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-
+function isEven(n){
+    if (n%2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
+function isOdd (n) {
+    if (n%2 !== 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
 /**
@@ -133,7 +235,11 @@
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
-
+function letterGrade (score,total) {
+    if (score >= 90) {
+        var result 
+    }
+}
 
 /**
  * Checks if a `restaurant` object has a `reviews` property.
